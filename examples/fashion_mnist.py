Fix numerical issues with Tanh and Sigmoid alpha_beta computations

examples/fashion_mnist.py
@@ -50,11 +50,12 @@ def train(net, args):
     transform, target_transform = construct_transform()
     train_data = datasets.FashionMNIST('../fashion_data', train=True, download=True,
                                        transform=transform, target_transform=target_transform)
-    train_loader = DataLoader(train_data, batch_size=100, shuffle=True, num_workers=8)
+    train_loader = DataLoader(train_data, batch_size=256, shuffle=True, num_workers=8)
 
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.AdamW(net.parameters())
+    optimizer = torch.optim.AdamW(net.parameters(), lr=5e-4)
 
+    k = 1.0
     for epoch in trange(10):
 
         running_loss = 0.0
@@ -67,11 +68,11 @@ def train(net, args):
 
             cross_entropy = criterion(y_hat, y)
 
-            epsilon = 0.01
-            bounds = net.crown_ibp_interval(X - epsilon, X + epsilon)
-            margin = adversarial_prob_margin(bounds, y).mean()
+            epsilon = 0.05
+            bounds = net.crown_interval(X - epsilon, X + epsilon)
+            logit = adversarial_logit(bounds, y)
 
-            loss = cross_entropy - margin
+            loss = k * cross_entropy + (1 - k) * criterion(logit, y)
             loss.backward()
             optimizer.step()
 
@@ -82,6 +83,27 @@ def train(net, args):
                 print(f'[{epoch + 1}, {i + 1:3d}] loss: {running_loss / 100:.3f}, cross entropy: {running_cross_entropy / 100:.3f}')
                 running_loss = 0.0
                 running_cross_entropy = 0.0
+
+        k = max(k - 0.1, 0.5)
+
+
+@torch.no_grad()
+def test(net, args):
+    transform, target_transform = construct_transform()
+    test_data = datasets.FashionMNIST('../fashion_data', train=False, download=True,
+                                      transform=transform, target_transform=target_transform)
+    test_loader = DataLoader(test_data, batch_size=100, shuffle=False, num_workers=8)
+
+    correct = 0
+    for i, (X, y) in enumerate(test_loader):
+        X, y = X.to(args.device), y.to(args.device)
+
+        y_hat = net(X)
+
+        predicted = torch.argmax(y_hat, 1)
+        correct += (predicted == y).sum().item()
+
+    print(f'Accuracy: {correct / len(test_data):.3f}')
 
 
 # torch.no_grad() has a huge impact on the memory consumption (due to the need to store intermediate tensors)
@@ -106,15 +128,7 @@ def timing(net, args):
             print(f'Out size: {out_size}, iterations: {iterations}, execution time: {exec_time}')
 
 
-def create_specification(y, num_classes=10):
-    C = torch.zeros((y.size(0), num_classes, num_classes), device=y.device)
-    C[:, torch.eye(num_classes)] = -1
-    C[y] = 0
-
-    return C
-
-
-def adversarial_prob_margin(y_hat, y):
+def adversarial_logit(y_hat, y):
     y_hat_lower, y_hat_upper = y_hat
 
     batch_size = y.size(0)
@@ -124,7 +138,16 @@ def adversarial_prob_margin(y_hat, y):
     adversarial_logit = y_hat_upper
     adversarial_logit[y_index] = y_hat_lower[y_index]
 
-    probs = F.softmax(adversarial_logit, dim=1)
+    return adversarial_logit
+
+
+def adversarial_prob_margin(y_hat, y):
+    batch_size = y.size(0)
+    y_index = (torch.arange(batch_size, device=y.device), y)
+
+    logit = adversarial_logit(y_hat, y)
+
+    probs = F.softmax(logit, dim=1)
     label_probs = probs.gather(1, y.unsqueeze(1))
 
     others_mask = torch.ones_like(probs, dtype=torch.bool)
@@ -153,7 +176,7 @@ def verify(net, args):
         margin_sum = 0.0
         for i, (X, y) in enumerate(test_loader):
             X, y = X.to(args.device), y.to(args.device)
-            epsilon = 0.01
+            epsilon = 0.05
             y_hat = method(X - epsilon, X + epsilon)
             worst_margin = adversarial_prob_margin(y_hat, y)
 
@@ -166,6 +189,7 @@ def main(args):
     net = FashionMNISTNetwork().to(args.device)
 
     train(net, args)
+    test(net, args)
     # timing(net, args)
     verify(net, args)
 

src/bound_propagation/alpha_beta.py
@@ -177,10 +177,10 @@ def alpha_beta_general(layer_bound: LayerBound, func: TensorFunction, derivative
     LB, UB = layer_bound
     n, p, np = regimes(LB, UB)
 
-    alpha_lower_k = torch.zeros_like(LB)
-    alpha_upper_k = torch.zeros_like(LB)
-    beta_lower_k = torch.zeros_like(LB)
-    beta_upper_k = torch.zeros_like(LB)
+    alpha_lower = torch.zeros_like(LB)
+    alpha_upper = torch.zeros_like(LB)
+    beta_lower = torch.zeros_like(LB)
+    beta_upper = torch.zeros_like(LB)
 
     LB_act, UB_act = func(LB), func(UB)
     LB_prime, UB_prime = derivative(LB), derivative(UB)
@@ -191,46 +191,47 @@ def alpha_beta_general(layer_bound: LayerBound, func: TensorFunction, derivative
 
     slope = (UB_act - LB_act) / (UB - LB)
 
+    def add_linear(alpha, beta, mask, a, x, y, a_mask=True):
+        if a_mask:
+            a = a[mask]
+
+        alpha[mask] = a
+        beta[mask] = y[mask] - a * x[mask]
+
     ###################
     # Negative regime #
     ###################
     # Upper bound
     # - Exact slope between LB and UB
-    alpha_upper_k[n] = slope[n]
-    beta_upper_k[n] = UB_act[n] - alpha_upper_k[n] * UB[n]
+    add_linear(alpha_upper, beta_upper, mask=n, a=slope, x=UB, y=UB_act)
 
     # Lower bound
     # - d = (LB + UB) / 2 for midpoint
     # - Slope is sigma'(d) and it has to cross through sigma(d)
-    alpha_lower_k[n] = d_prime[n]
-    beta_lower_k[n] = d_act[n] - alpha_lower_k[n] * d[n]
+    add_linear(alpha_lower, beta_lower, mask=n, a=d_prime, x=d, y=d_act)
 
     ###################
     # Positive regime #
     ###################
     # Lower bound
     # - Exact slope between LB and UB
-    alpha_lower_k[p] = slope[p]
-    beta_lower_k[p] = LB_act[p] - alpha_lower_k[p] * LB[p]
+    add_linear(alpha_lower, beta_lower, mask=p, a=slope, x=LB, y=LB_act)
 
     # Upper bound
     # - d = (LB + UB) / 2 for midpoint
     # - Slope is sigma'(d) and it has to cross through sigma(d)
-    alpha_upper_k[p] = d_prime[p]
-    beta_upper_k[p] = d_act[p] - alpha_upper_k[p] * d[p]
+    add_linear(alpha_upper, beta_upper, mask=p, a=d_prime, x=d, y=d_act)
 
     #################
     # Crossing zero #
     #################
     # Upper bound #
-    UB_prime_at_LB = UB_prime * (LB - UB) + UB_act
-
     # If tangent to UB is below LB, then take direct slope between LB and UB
-    direct_upper = np & (UB_prime_at_LB <= 0)
-    alpha_upper_k[direct_upper] = slope[direct_upper]
+    direct_upper = np & (slope <= UB_prime)
+    add_linear(alpha_upper, beta_upper, mask=direct_upper, a=slope, x=LB, y=LB_act)
 
     # Else use bisection to find upper bound on slope.
-    implicit_upper = np & (UB_prime_at_LB > 0)
+    implicit_upper = np & (slope > UB_prime)
 
     def f_upper(d: torch.Tensor) -> torch.Tensor:
         a_slope = (func(d) - func(LB[implicit_upper])) / (d - LB[implicit_upper])
@@ -240,20 +241,16 @@ def alpha_beta_general(layer_bound: LayerBound, func: TensorFunction, derivative
     # Bisection will return left and right bounds for d s.t. f_upper(d) is zero
     # Derivative of left bound will overapproximate the slope - hence a true bound
     d_upper, _ = bisection(torch.zeros_like(UB[implicit_upper]), UB[implicit_upper], f_upper)
-
     # Slope has to attach to (LB, sigma(LB))
-    alpha_upper_k[implicit_upper] = derivative(d_upper)
-    beta_upper_k[np] = LB_act[np] - alpha_upper_k[np] * LB[np]
+    add_linear(alpha_upper, beta_upper, mask=implicit_upper, a=derivative(d_upper), x=LB, y=LB_act, a_mask=False)
 
     # Lower bound #
-    LB_prime_at_UB = LB_prime * (UB - LB) + LB_act
-
     # If tangent to LB is above UB, then take direct slope between LB and UB
-    direct_lower = np & (LB_prime_at_UB >= 0)
-    alpha_lower_k[direct_lower] = slope[direct_lower]
+    direct_lower = np & (slope <= LB_prime)
+    add_linear(alpha_lower, beta_lower, mask=direct_lower, a=slope, x=UB, y=UB_act)
 
     # Else use bisection to find upper bound on slope.
-    implicit_lower = np & (LB_prime_at_UB < 0)
+    implicit_lower = np & (slope > LB_prime)
 
     def f_lower(d: torch.Tensor) -> torch.Tensor:
         a_slope = (func(UB[implicit_lower]) - func(d)) / (UB[implicit_lower] - d)
@@ -263,11 +260,10 @@ def alpha_beta_general(layer_bound: LayerBound, func: TensorFunction, derivative
     # Bisection will return left and right bounds for d s.t. f_lower(d) is zero
     # Derivative of right bound will overapproximate the slope - hence a true bound
     _, d_lower = bisection(LB[implicit_lower], torch.zeros_like(LB[implicit_lower]), f_lower)
+    # Slope has to attach to (UB, sigma(UB))
+    add_linear(alpha_lower, beta_lower, mask=implicit_lower, a=derivative(d_lower), x=UB, y=UB_act, a_mask=False)
 
-    alpha_lower_k[implicit_lower] = derivative(d_lower)
-    beta_lower_k[np] = UB_act[np] - alpha_lower_k[np] * UB[np]
-
-    return (alpha_lower_k, alpha_upper_k), (beta_lower_k, beta_upper_k)
+    return (alpha_lower, alpha_upper), (beta_lower, beta_upper)
 
 
 def bisection(l: torch.Tensor, h: torch.Tensor, f: TensorFunction, num_iter: int = 20) -> Tuple[torch.Tensor, torch.Tensor]: