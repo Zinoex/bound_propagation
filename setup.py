3dce46144d7eba051cecc70630914d62de072ef7
Frederik Baymler Mathiesen <frederik@baymler.com>
2022-03-30 11:26:03 +0200

    Bump version with new module

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="bound_propagation",
-    version="0.2.4",
+    version="0.2.5",
     author="Frederik Baymler Mathiesen",
     author_email="frederik@baymler.com",
     description="Bound propagation in Pytorch",
