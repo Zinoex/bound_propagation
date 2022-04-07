cd5421654a6237165bc88fa9bb060241ffbb298b
Frederik Baymler Mathiesen <frederik@baymler.com>
2022-04-07 11:52:29 +0200

    Publish new module types

--- src/bound_propagation/__init__.py
+++ src/bound_propagation/__init__.py
@@ -6,3 +6,6 @@ from .linear import BoundLinear
 from .sequential import BoundSequential
 from .residual import Residual, BoundResidual
 from .cat import Cat, BoundCat
+from .parallel import Parallel, BoundParallel
+from .bivariate import Add, Sub, BoundAdd, BoundSub
+from .saturation import Clamp, BoundClamp
