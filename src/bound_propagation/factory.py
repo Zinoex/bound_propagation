5521abb8a5b8c3ebd237f74909f5c92eaf513a88
Frederik Mathiesen <frederik_mathies@hotmail.com>
2022-05-08 17:56:27 +0200

    Change factory to prepend items to class mapping to allow overriding or specializing BoundModules

--- src/bound_propagation/factory.py
+++ src/bound_propagation/factory.py
@@ -18,28 +18,28 @@ class BoundModelFactory:
     you can add the new subtype to the factory, and when constructing BoundSequential, this will be recognized.
     """
     def __init__(self, **kwargs):
-        self.class_mapping = {
-            nn.Sequential: BoundSequential,
-            nn.Linear: BoundLinear,
-            nn.ReLU: BoundReLU,
-            nn.Tanh: BoundTanh,
-            nn.Sigmoid: BoundSigmoid,
-            nn.Identity: BoundIdentity,
-            Add: BoundAdd,
-            Sub: BoundSub,
-            Residual: BoundResidual,
-            Cat: BoundCat,
-            Parallel: BoundParallel,
-            Clamp: BoundClamp
-        }
+        self.class_mapping = [
+            (nn.Sequential, BoundSequential),
+            (nn.Linear, BoundLinear),
+            (nn.ReLU, BoundReLU),
+            (nn.Tanh, BoundTanh),
+            (nn.Sigmoid, BoundSigmoid),
+            (nn.Identity, BoundIdentity),
+            (Add, BoundAdd),
+            (Sub, BoundSub),
+            (Residual, BoundResidual),
+            (Cat, BoundCat),
+            (Parallel, BoundParallel),
+            (Clamp, BoundClamp)
+        ]
 
         self.kwargs = kwargs
 
     def register(self, original_class, bound_class):
-        self.class_mapping[original_class] = bound_class
+        self.class_mapping.insert(0, (original_class, bound_class))
 
     def build(self, module):
-        for orig_class, bound_class in self.class_mapping.items():
+        for orig_class, bound_class in self.class_mapping:
             if isinstance(module, orig_class):
                 return bound_class(module, self, **self.kwargs)
 
