478761d3b37da29fb29f19f67cf2b95b1ddfb4ba
Frederik Mathiesen <frederik_mathies@hotmail.com>
2022-08-17 12:59:46 +0200

    WIP: alpha-CROWN

--- src/bound_propagation/general.py
+++ src/bound_propagation/general.py
@@ -2,6 +2,8 @@ import abc
 
 import torch
 from torch import nn
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ExponentialLR
 
 from .bounds import LinearBounds, IntervalBounds
 
@@ -10,6 +12,11 @@ class BoundModule(nn.Module, abc.ABC):
     def __init__(self, module, factory, **kwargs):
         super().__init__()
         self.module = module
+
+        self.optimizer = kwargs.get('bounds_optimizer', Adam)
+        self.bounds_iterations = kwargs.get('bounds_iterations', 20)
+        self.lr = kwargs.get('bounds_lr', 1e-3)
+        self.lr_decay = kwargs.get('bounds_lr_decay', 0.97)
 
     @torch.no_grad()
     def crown_relax(self, region):
@@ -24,13 +31,23 @@ class BoundModule(nn.Module, abc.ABC):
         bounds = IntervalBounds(region, region.lower, region.upper)
         self.ibp_forward(bounds, save_relaxation=True)
 
-    def crown_with_relaxation(self, relax, region, bound_lower, bound_upper):
+    def crown(self, region, bound_lower=True, bound_upper=True, alpha=False):
+        return self.crown_with_relaxation(self.crown_relax, region, bound_lower, bound_upper, alpha)
+
+    def crown_ibp(self, region, bound_lower=True, bound_upper=True, alpha=False):
+        return self.crown_with_relaxation(self.ibp_relax, region, bound_lower, bound_upper, alpha)
+
+    def crown_with_relaxation(self, relax, region, bound_lower, bound_upper, alpha):
         out_size = self.propagate_size(region.lower.size(-1))
 
         relax(region)
 
-        linear_bounds = self.initial_linear_bounds(region, out_size, lower=bound_lower, upper=bound_upper)
-        linear_bounds = self.crown_backward(linear_bounds)
+        if alpha:
+            linear_bounds = self.alpha_crown(region, out_size, bound_lower, bound_upper)
+        else:
+            linear_bounds = self.initial_linear_bounds(region, out_size, lower=bound_lower, upper=bound_upper)
+            linear_bounds = self.crown_backward(linear_bounds)
+
         self.clear_relaxation()
         return linear_bounds
 
@@ -46,11 +63,41 @@ class BoundModule(nn.Module, abc.ABC):
         linear_bounds = LinearBounds(region, lower, upper)
         return linear_bounds
 
-    def crown(self, region, bound_lower=True, bound_upper=True):
-        return self.crown_with_relaxation(self.crown_relax, region, bound_lower, bound_upper)
+    def alpha_crown(self, region, out_size, bound_lower, bound_upper):
+        params = self.bound_parameters()
 
-    def crown_ibp(self, region, bound_lower=True, bound_upper=True):
-        return self.crown_with_relaxation(self.ibp_relax, region, bound_lower, bound_upper)
+        if bound_lower:
+            lower = self.optimize_bounds(region, out_size, True, params).lower
+        else:
+            lower = None
+
+        if bound_upper:
+            upper = self.optimize_bounds(region, out_size, False, params).upper
+        else:
+            upper = None
+
+        return LinearBounds(region, lower, upper)
+
+    def optimize_bounds(self, region, out_size, lower, params):
+        optimizer = self.optimizer(params, self.lr)
+        scheduler = ExponentialLR(optimizer, self.lr_decay)
+
+        self.reset_params()
+
+        for iteration in range(self.bounds_iterations):
+            linear_bounds = self.initial_linear_bounds(region, out_size, lower=lower, upper=not lower)
+            linear_bounds = self.crown_backward(linear_bounds)
+            interval_bounds = linear_bounds.concretize()
+
+            loss = (-1 if lower else 1) * interval_bounds.lower.sum()
+
+            optimizer.zero_grad(set_to_none=True)
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+        linear_bounds = self.initial_linear_bounds(region, out_size, lower=lower, upper=not lower)
+        return self.crown_backward(linear_bounds)
 
     @property
     @abc.abstractmethod
@@ -93,3 +140,9 @@ class BoundModule(nn.Module, abc.ABC):
         # references down the bound module graph corresponding to the model graph.
         # Then loading the state dict at the top level automatically populates the weights in all sub-bound modules.
         self.module.load_state_dict(state_dict=state_dict, strict=strict)
+
+    def bound_parameters(self):
+        raise NotImplementedError()
+
+    def reset_params(self):
+        raise NotImplementedError()
