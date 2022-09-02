Fix non-zeroed gradients in alpha-CROWN and simplify optimization (no more reset_params)

src/bound_propagation/activation.py
@@ -150,20 +150,11 @@ class BoundReLU(BoundActivation):
         super().__init__(module, factory)
         self.adaptive_relu = adaptive_relu
 
-        self.unstable_lower, self._unstable_slope_lower, self.initial_unstable_slope_lower = None, None, None
-
-    @property
-    def unstable_slope_lower(self):
-        return self._unstable_slope_lower
-
-    @unstable_slope_lower.setter
-    def unstable_slope_lower(self, value):
-        self._unstable_slope_lower = value
-        self.initial_unstable_slope_lower = value
+        self.unstable_lower, self.unstable_slope_lower = None, None
 
     def clear_relaxation(self):
         super().clear_relaxation()
-        self.unstable_lower, self._unstable_slope_lower, self.initial_unstable_slope_lower = None, None, None
+        self.unstable_lower, self.unstable_slope_lower = None, None
 
     @assert_bound_order
     def alpha_beta(self, preactivation):
@@ -233,9 +224,6 @@ class BoundReLU(BoundActivation):
         else:
             logger.warning('ReLU bound not parameterized but expected to')
 
-    def reset_params(self):
-        self.unstable_slope_lower.data.copy_(self.initial_unstable_slope_lower)
-
     def clip_params(self):
         self.unstable_slope_lower.data.clamp_(min=0, max=1)
 
@@ -244,33 +232,13 @@ class BoundSigmoid(BoundActivation):
     def __init__(self, module, factory, **kwargs):
         super().__init__(module, factory)
 
-        self.unstable_lower, self._unstable_d_lower, self.initial_unstable_d_lower = None, None, None
-        self.unstable_upper, self._unstable_d_upper, self.initial_unstable_d_upper = None, None, None
-        self.unstable_range_lower, self.unstable_range_upper = None, None
-
-    @property
-    def unstable_d_lower(self):
-        return self._unstable_d_lower
-
-    @unstable_d_lower.setter
-    def unstable_d_lower(self, value):
-        self._unstable_d_lower = value
-        self.initial_unstable_d_lower = value
-
-    @property
-    def unstable_d_upper(self):
-        return self._unstable_d_upper
-
-    @unstable_d_upper.setter
-    def unstable_d_upper(self, value):
-        self._unstable_d_upper = value
-        self.initial_unstable_d_upper = value
+        self.unstable_lower, self.unstable_d_lower, self.unstable_range_lower = None, None, None
+        self.unstable_upper, self.unstable_d_upper, self.unstable_range_upper = None, None, None
 
     def clear_relaxation(self):
         super().clear_relaxation()
-        self.unstable_lower, self._unstable_d_lower, self.initial_unstable_d_lower = None, None, None
-        self.unstable_upper, self._unstable_d_upper, self.initial_unstable_d_upper = None, None, None
-        self.unstable_range_lower, self.unstable_range_upper = None, None
+        self.unstable_lower, self.unstable_d_lower, self.unstable_range_lower = None, None, None
+        self.unstable_upper, self.unstable_d_upper, self.unstable_range_upper = None, None, None
 
     def derivative(self, x):
         return torch.sigmoid(x) * (1 - torch.sigmoid(x))
@@ -438,10 +406,6 @@ class BoundSigmoid(BoundActivation):
         yield self.unstable_d_lower
         yield self.unstable_d_upper
 
-    def reset_params(self):
-        self.unstable_d_lower.data.copy_(self.initial_unstable_d_lower)
-        self.unstable_d_upper.data.copy_(self.initial_unstable_d_upper)
-
     def clip_params(self):
         self.unstable_d_lower.data.clamp_(min=self.unstable_range_lower[0], max=self.unstable_range_lower[1])
         self.unstable_d_upper.data.clamp_(min=self.unstable_range_upper[0], max=self.unstable_range_upper[1])
@@ -497,22 +461,11 @@ class BoundExp(BoundActivation):
     def __init__(self, module, factory, **kwargs):
         super().__init__(module, factory)
 
-        self.unstable_lower, self._unstable_d_lower, self.initial_unstable_d_lower = None, None, None
-        self.unstable_range_lower = None
-
-    @property
-    def unstable_d_lower(self):
-        return self._unstable_d_lower
-
-    @unstable_d_lower.setter
-    def unstable_d_lower(self, value):
-        self._unstable_d_lower = value
-        self.initial_unstable_d_lower = value
+        self.unstable_lower, self.unstable_d_lower, self.unstable_range_lower = None, None, None
 
     def clear_relaxation(self):
         super().clear_relaxation()
-        self.unstable_lower, self._unstable_d_lower, self.initial_unstable_d_lower = None, None, None
-        self.unstable_range_lower = None
+        self.unstable_lower, self.unstable_d_lower, self.unstable_range_lower = None, None, None
 
     def derivative(self, x):
         return x.exp()
@@ -582,9 +535,6 @@ class BoundExp(BoundActivation):
 
         yield self.unstable_d_lower
 
-    def reset_params(self):
-        self.unstable_d_lower.data.copy_(self.initial_unstable_d_lower)
-
     def clip_params(self):
         self.unstable_d_lower.data.clamp_(min=self.unstable_range_lower[0], max=self.unstable_range_lower[1])
 
@@ -599,22 +549,11 @@ class BoundLog(BoundActivation):
     def __init__(self, module, factory, **kwargs):
         super().__init__(module, factory)
 
-        self.unstable_upper, self._unstable_d_upper, self.initial_unstable_d_upper = None, None, None
-        self.unstable_range_upper = None
-
-    @property
-    def unstable_d_upper(self):
-        return self._unstable_d_upper
-
-    @unstable_d_upper.setter
-    def unstable_d_upper(self, value):
-        self._unstable_d_upper = value
-        self.initial_unstable_d_upper = value
+        self.unstable_upper, self.unstable_d_upper, self.unstable_range_upper = None, None, None
 
     def clear_relaxation(self):
         super().clear_relaxation()
-        self.unstable_upper, self._unstable_d_upper, self.initial_unstable_d_upper = None, None, None
-        self.unstable_range_upper = None
+        self.unstable_upper, self.unstable_d_upper, self.unstable_range_upper = None, None, None
 
     def derivative(self, x):
         # As we already assume x > 0, this is well-defined
@@ -687,9 +626,6 @@ class BoundLog(BoundActivation):
 
         yield self.unstable_d_upper
 
-    def reset_params(self):
-        self.unstable_d_upper.data.copy_(self.initial_unstable_d_upper)
-
     def clip_params(self):
         self.unstable_d_upper.data.clamp_(min=self.unstable_range_upper[0], max=self.unstable_range_upper[1])
 
@@ -705,33 +641,13 @@ class BoundReciprocal(BoundActivation):
     def __init__(self, module, factory, **kwargs):
         super().__init__(module, factory)
 
-        self.unstable_lower, self._unstable_d_lower, self.initial_unstable_d_lower = None, None, None
-        self.unstable_upper, self._unstable_d_upper, self.initial_unstable_d_upper = None, None, None
-        self.unstable_range_lower, self.unstable_range_upper = None, None
-
-    @property
-    def unstable_d_lower(self):
-        return self._unstable_d_lower
-
-    @unstable_d_lower.setter
-    def unstable_d_lower(self, value):
-        self._unstable_d_lower = value
-        self.initial_unstable_d_lower = value
-
-    @property
-    def unstable_d_upper(self):
-        return self._unstable_d_upper
-
-    @unstable_d_upper.setter
-    def unstable_d_upper(self, value):
-        self._unstable_d_upper = value
-        self.initial_unstable_d_upper = value
+        self.unstable_lower, self.unstable_d_lower, self.unstable_range_lower = None, None, None
+        self.unstable_upper, self.unstable_d_upper, self.unstable_range_upper = None, None, None
 
     def clear_relaxation(self):
         super().clear_relaxation()
-        self.unstable_lower, self._unstable_d_lower, self.initial_unstable_d_lower = None, None, None
-        self.unstable_upper, self._unstable_d_upper, self.initial_unstable_d_upper = None, None, None
-        self.unstable_range_lower, self.unstable_range_upper = None, None
+        self.unstable_lower, self.unstable_d_lower, self.unstable_range_lower = None, None, None
+        self.unstable_upper, self.unstable_d_upper, self.unstable_range_upper = None, None, None
 
     def derivative(self, x):
         return -1 / (x ** 2)
@@ -845,10 +761,6 @@ class BoundReciprocal(BoundActivation):
         yield self.unstable_d_lower
         yield self.unstable_d_upper
 
-    def reset_params(self):
-        self.unstable_d_lower.data.copy_(self.initial_unstable_d_lower)
-        self.unstable_d_upper.data.copy_(self.initial_unstable_d_upper)
-
     def clip_params(self):
         self.unstable_d_lower.data.clamp_(min=self.unstable_range_lower[0], max=self.unstable_range_lower[1])
         self.unstable_d_upper.data.clamp_(min=self.unstable_range_upper[0], max=self.unstable_range_upper[1])
@@ -920,33 +832,13 @@ class BoundSin(BoundActivation):
     def __init__(self, module, factory, **kwargs):
         super().__init__(module, factory)
 
-        self.unstable_lower, self._unstable_d_lower, self.initial_unstable_d_lower = None, None, None
-        self.unstable_upper, self._unstable_d_upper, self.initial_unstable_d_upper = None, None, None
-        self.unstable_range_lower, self.unstable_range_upper = None, None
-
-    @property
-    def unstable_d_lower(self):
-        return self._unstable_d_lower
-
-    @unstable_d_lower.setter
-    def unstable_d_lower(self, value):
-        self._unstable_d_lower = value
-        self.initial_unstable_d_lower = value
-
-    @property
-    def unstable_d_upper(self):
-        return self._unstable_d_upper
-
-    @unstable_d_upper.setter
-    def unstable_d_upper(self, value):
-        self._unstable_d_upper = value
-        self.initial_unstable_d_upper = value
+        self.unstable_lower, self.unstable_d_lower, self.unstable_range_lower = None, None, None
+        self.unstable_upper, self.unstable_d_upper, self.unstable_range_upper = None, None, None
 
     def clear_relaxation(self):
         super().clear_relaxation()
-        self.unstable_lower, self._unstable_d_lower, self.initial_unstable_d_lower = None, None, None
-        self.unstable_upper, self._unstable_d_upper, self.initial_unstable_d_upper = None, None, None
-        self.unstable_range_lower, self.unstable_range_upper = None, None
+        self.unstable_lower, self.unstable_d_lower, self.unstable_range_lower = None, None, None
+        self.unstable_upper, self.unstable_d_upper, self.unstable_range_upper = None, None, None
 
     def derivative(self, x):
         return x.cos()
@@ -1204,10 +1096,6 @@ class BoundSin(BoundActivation):
 
         yield self.unstable_d_lower
         yield self.unstable_d_upper
-
-    def reset_params(self):
-        self.unstable_d_lower.data.copy_(self.initial_unstable_d_lower)
-        self.unstable_d_upper.data.copy_(self.initial_unstable_d_upper)
 
     def clip_params(self):
         self.unstable_d_lower.data.clamp(min=self.unstable_range_lower[0], max=self.unstable_range_lower[1])

src/bound_propagation/general.py
@@ -17,10 +17,9 @@ class BoundModule(nn.Module, abc.ABC):
         super().__init__()
         self.module = module
 
-        self.optimizer = kwargs.get('bounds_optimizer', Adam)
-        self.bounds_iterations = kwargs.get('bounds_iterations', 40)
-        self.lr = kwargs.get('bounds_lr', 1e-2)
-        self.lr_decay = kwargs.get('bounds_lr_decay', 0.97)
+        self.alpha_optimizer = kwargs.get('alpha_optimizer', Adam)
+        self.alpha_iterations = kwargs.get('alpha_iterations', 20)
+        self.alpha_lr = kwargs.get('alpha_lr', 1.0)
 
     @torch.no_grad()
     def crown_relax(self, region):
@@ -71,52 +70,48 @@ class BoundModule(nn.Module, abc.ABC):
         linear_bounds = LinearBounds(region, lower, upper)
         return linear_bounds
 
-    @torch.enable_grad()
     def alpha_crown(self, region, out_size, bound_lower, bound_upper):
         params = list(self.bound_parameters())
 
         if all([param.numel() == 0 for param in params]):
             logger.warning('No parameters available for alpha-CROWN. Check architecture of network.')
 
-            with torch.no_grad():
-                linear_bounds = self.initial_linear_bounds(region, out_size, lower=bound_lower, upper=bound_upper)
-                return self.crown_backward(linear_bounds, False)
+            linear_bounds = self.initial_linear_bounds(region, out_size, lower=bound_lower, upper=bound_upper)
+            return self.crown_backward(linear_bounds, False)
 
-        if bound_lower:
-            lower = self.optimize_bounds(region, out_size, True, params).lower
-        else:
-            lower = None
+        self.optimize_bounds(region, out_size, params, bound_lower=bound_lower, bound_upper=bound_upper)
 
-        if bound_upper:
-            upper = self.optimize_bounds(region, out_size, False, params).upper
-        else:
-            upper = None
+        linear_bounds = self.initial_linear_bounds(region, out_size, lower=bound_lower, upper=bound_upper)
+        return self.crown_backward(linear_bounds, True)
 
-        return LinearBounds(region, lower, upper)
+    @torch.enable_grad()
+    def optimize_bounds(self, region, out_size, params, bound_lower=True, bound_upper=True):
+        optimizer = self.alpha_optimizer(params, self.alpha_lr)
 
-    def optimize_bounds(self, region, out_size, lower, params):
-        optimizer = self.optimizer(params, self.lr)
-        scheduler = ExponentialLR(optimizer, self.lr_decay)
-
-        self.reset_params()
-
-        for iteration in range(self.bounds_iterations):
-            linear_bounds = self.initial_linear_bounds(region, out_size, lower=lower, upper=not lower)
+        for iteration in range(self.alpha_iterations):
+            linear_bounds = self.initial_linear_bounds(region, out_size, lower=bound_lower, upper=bound_upper)
             linear_bounds = self.crown_backward(linear_bounds, True)
             interval_bounds = linear_bounds.concretize()
 
-            loss = (-interval_bounds.lower if lower else interval_bounds.upper).mean()
+            loss = 0
+            if bound_lower:
+                # Use sum for aggregation because bound parameters are per sample, hence we need to scale the loss with
+                # the number of samples
+                loss = -interval_bounds.lower.sum()
 
-            optimizer.zero_grad(set_to_none=True)
+            if bound_upper:
+                loss = interval_bounds.upper.sum()
+
+            optimizer.zero_grad(set_to_none=True)  # Bound parameters
+            self.zero_grad(set_to_none=True)       # Network/model parameters
+
             loss.backward()
             optimizer.step()
-            scheduler.step()
 
             self.clip_params()  # Projected Gradient Descent
 
-        with torch.no_grad():
-            linear_bounds = self.initial_linear_bounds(region, out_size, lower=lower, upper=not lower)
-            return self.crown_backward(linear_bounds, True)
+        optimizer.zero_grad(set_to_none=True)
+        self.zero_grad(set_to_none=True)
 
     @property
     @abc.abstractmethod
@@ -162,9 +157,6 @@ class BoundModule(nn.Module, abc.ABC):
 
     def bound_parameters(self):
         return []
-
-    def reset_params(self):
-        pass
 
     def clip_params(self):
         pass